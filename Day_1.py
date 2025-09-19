a7ca1ac8c49d259e10be5c02ccb234c027d1270b
Arpita Sah <arpitasah750@gmail.com>
2025-09-19 22:16:03 +0545

    Create Day_1.py
    In the Day-1 of python coding. Here, I written the related code using the print() function. And using print() function how can we print string is describe in brief in this day-1 python code. Like the use of single, double, and triple inverted comma for printing the string or sentences using the print() function in python.

---
+++ Day_1.py
@@ -0,0 +1,28 @@
+# How to use print() function on Python
+# For instance, if I want to print "Hello World!" in Python then I will use print() to print it.
+# The first prompt program in the python.
+print("Hello World!") # Output: Hello World!
+# So, from the first code you can understand that if you write anything inside the python print() function and you will get the input as a output.
+# In the condition to print the string inside the print function you can use print() function and inside print function you have to use the single, double, and triple inverted comma.
+
+# For single inverted comma
+print('Hi everyone, This is my first day in python coding')
+# Output: Hi everyone, This is my first day in python coding
+
+# For double inverted comma
+print("Hey, It's me Arpita Sah, and from today I started doing coding in Python.")
+# Output: Hey, It's me Arpita Sah, and from today I started doing coding in Python.
+# The use of double inverted comma is that if you want to print it's inside the single inverted comma you will get error but if you will use the double inverted comma it will provide you actual sentence and string.
+
+# For triple inverted comma
+print("""
+Hello, I am Arpita.
+From BscIT.
+And I started learning Python.
+""")
+# Output: Hello, I am Arpita.
+#         From BscIT.
+#         And I started learning Python.
+# The use of triple inverted comma in print function is used to print the multiple statement sentences of string in the python as above.
+
+# This is the Day-1 in python Coding.
