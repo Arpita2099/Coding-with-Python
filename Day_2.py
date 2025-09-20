f2eb309e467a67f8e4facf67d998c64454b32762
Arpita Sah <arpitasah750@gmail.com>
2025-09-20 12:44:00 +0545

    Create Day_2.py
    In the Day-2 of Python coding, I describe in details about the print() function that how print() function can print the various of datatypes and to know which datatypes the print() function print, I used type() function inside the print function. So, this is all about the print() function uses and important role in the python. In addition, I also used the comment in the python with "#" to make the python code readable and understandable that what this code is about.

---
+++ Day_2.py
@@ -0,0 +1,36 @@
+# In the Day-2 of python Coding. We will learn how to use print() function to print other datatypes like integer, float, and bollen, etc.
+# How to print integer using print() function in python.
+# For positive integer
+print(2) # Output: 2
+
+# For negative integer
+print(-4) # Output:-4
+
+# How to print the float using print() function in python.
+# for positive float
+print(12.6) # Output: 12.6
+
+# for negative float
+print(-9.9) # Output: -9.9
+
+# How to print the boolean using the print() function in python.
+print(True) # Output: True
+print(False) # Output: False
+
+# How to print list using print() function in python.
+print([1,2,3,4,5,6]) # Output: [1,2,3,4,5,6]
+print(type([1,2,3,4,5,6])) # Output: <class 'list'>
+
+# How to print tuple using print() function in python.
+print((2,4,6,8,10,12)) # Output: (2,4,6,8,10,12)
+print(type((2,4,6,8,10,12))) # Output: <classs 'tuple'>
+
+# How to print set using print() function in python.
+print({1,3,5,7,9,11}) # Output: 1,3,5,7,9,11}
+print(type({1,3,5,7,9,11})) # Output: <class 'set'>
+
+# How to print dictionary using print() function in python.
+print({'Name':'Arpita','Tiltle':'Sah'}) # Output: {'Name':'Arpita','Tiltle':'Sah'}
+print(type({'Name':'Arpita','Tiltle':'Sah'})) # Output: <class 'dict'>
+
+# So, in the day-2 of this python code. Here, the use of print() function for datatypes.
