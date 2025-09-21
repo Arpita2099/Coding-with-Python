806c6892ee1d353e20ad41da340751f36f9fd595
Arpita Sah <arpitasah750@gmail.com>
2025-09-21 21:43:59 +0545

    Create Day_3.py
    Today, I have written code related to python variable and datatype. How we can store any thing in variable and also print using print() function in the python. That's all about today coding with python.

---
+++ Day_3.py
@@ -0,0 +1,92 @@
+# Variable and Datatype
+a=1 # a is a variable and also it is posite integer type of data.
+print(a) 
+print(type(a))
+# Output:
+# 1
+# <class 'int'>
+ 
+b=-7 # b is a variable and also it is a negative integer type of data.
+print(b)
+print(type(b))
+# Output:
+# -7
+# <class 'int'>
+
+c=45.8 # c is a variable and also it is a positive float type of data.
+print(c)
+print(type(c))
+# Output:
+# -45.8
+# <class 'float'>
+
+d=-7.5 # d is a variable and also it is a negative float type of data.
+print(d)
+print(type(d))
+# Output:
+# -7.5
+# <class 'float'>
+
+e=True
+f=False
+# These both are variable of boolen data type
+print(e)
+print(f)
+print(type(e))
+# Output:
+# True
+# False
+# <class 'bool'>
+
+l=[5,10,15,20] # l is a variable which is list type of data.
+print(l)
+print(type(l))
+# Output:
+# [5, 10, 15, 20]
+# <class 'list'>
+
+t=(1,2,3,4,5) # t is a variable which is tuple type of data.
+print(t)
+print(type(t))
+# Output:
+# (1, 2, 3, 4, 5)
+# <class 'tuple'>
+
+s={6,7,8,9} # s is a variable which is set type of data.
+print(s)
+print(type(s))
+# Output:
+# {8, 9, 6, 7}
+# <class 'set'>
+
+d={'Name':'Arpita','Gender':'Femal'} # d is a variable which is dictinory type of data.
+print(d)
+print(type(d))
+# Output:
+# {'Name': 'Arpita', 'Gender': 'Femal'}
+# <class 'dict'>
+
+str='Arpita Sah'
+str1="It's me Arpita Sah."
+str2=""""This is my confort place", okay keep in you mind"""
+# These are the variables with the string type of data.
+print(str)
+print(str1)
+print(str2)
+print(type(str2))
+#Output:
+# Arpita Sah
+# It's me Arpita Sah.
+# "This is my confort place", okay keep in you mind
+# <class 'str'>
+
+comp=(4+6j) # comp is a variable which is comples type of data.
+print(comp)
+print(type(comp))
+# Output:
+# (4+6j)
+# <class 'complex'>
+
+# Why data types is required: because to print the different types of data in the python.
+# Also, importance while doing concatination because while concatination they need the similar types of datatype then only it can perform the concatination in the python.
+# And to know that the data type are similar or not you can use the type() function inside the print() function to find the types of data types.
