be956a01232f7b0b5b4ea59751c7a16500039038
Arpita Sah <arpitasah750@gmail.com>
2025-09-22 20:58:02 +0545

    Create Day_4.py
    In this Day_4 of python code, I have given detail explanation about the operators used in python.

---
+++ Day_4.py
@@ -0,0 +1,40 @@
+# In the Day-4 of python code.
+# Going to learn about the operators in Python.
+
+# Additional Operators
+a=10
+b=20
+print(a+b)
+# Substraction Operators
+print(b-a)
+
+# Multiplication Operators
+print(a*2)
+
+# Divisible Operators
+print(b/a)
+
+# Modulus Operators
+print(5%3)
+
+# Floor Division Operators
+print(a//b)
+print(15//6)
+
+# Exponential Operators
+print(2**3)
+
+# Create a calculator that can operator the calculation of two number.
+a=30
+b=20
+print(f"Addtion of {a} and {b} is: ",a+b)
+print(f"Substraction of {a} and {b} is: ",a-b)
+print(f"Multiplication of {a} and {b} is: ",a*b)
+print(f"Division of {a} and {b} is: ",a/b)
+print(f"Floor Division of {a} and {b} is: ",a//b)
+print(f"Square of {a} and {b} is: ",a**b)
+print(f"Modulus of {a} and {b} is: ",a%b)
+# Here, I created the calculator using the variable and operator. We can also make by using int, intput() function and if, elif but with upto now level.
+# I am using the simple way to make calculator which seem that I can step my way step by step from basic to advanced.
+# So, today the coding of python is only based on the operators of the python.
+# This is all about today coding.
