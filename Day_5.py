9628eca646b36bdebf31603eb3225fb646e87e4d
Arpita Sah <arpitasah750@gmail.com>
2025-09-23 21:35:58 +0545

    Create Day_5.py
    In the Day_5 of python coding. I solve some of the question using the variable, data types, operators, and print(). So, this question made me confident that I can do python in good way.

---
+++ Day_5.py
@@ -0,0 +1,49 @@
+# In the day_5 of the coding I am going to solve some of the using print() function, variable, data types, and operators.
+# As I am in my beginner level of the python coding I go through doing the simple problem to make regular practice in the coding.
+
+# Write a program in python to calculate the area of rectangle.
+length=12
+breadth=13
+Area_of_rectangle=(length*breadth)
+print("Area of the rectangle is: ",Area_of_rectangle)
+
+# Write a program in the python to calculate the area of circle.
+pie=3.14
+radius=14
+Area_of_circle=2*pie*(radius**2)
+print("Area of Circle is: ",float(Area_of_circle))
+
+# Write a program in the python to find the sum of three number and their average.
+num1=10
+num2=20
+num3=30
+sum=num1+num2+num3
+Average=sum/3
+print("Sum of the three number is: ",sum)
+print("Average of the three number: ",Average)
+
+# Write a program in the python to find the interest were principle, rate, and time is given.
+principle=1000
+rate=4
+time=5
+Interest=(principle*rate*time)/100
+print("The interest is: ",Interest)
+
+# Write a program to check that the given number is even.
+num=12
+print(f"{num} is even number: ",num%2==0)
+
+# Write a program in the python which can convert the celcius into farenheit.
+celcius=50
+farenheit=(celcius*9/5)+32
+print(f"{celcius} into farenheit is: {farenheit}")
+
+# String concatination.
+A="Arpita"
+s="Sah"
+print(A+" "+s)
+
+# String Multiplication
+str="Hello Ji, Kaise hai aap sabhi."
+print(str*10)
+
