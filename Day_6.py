c4f6c10cc25bff8f3381a3583cf7d45439faf18e
Arpita Sah <arpitasah750@gmail.com>
2025-09-24 21:19:19 +0545

    Create Day_6.py
    In today coding, I code for the Typecasting in the python.

---
+++ Day_6.py
@@ -0,0 +1,23 @@
+# Typecasting in Python
+# The converstion of one data type into the other data type is known as type casting in python or type conversion in python.
+# It also support the various function.
+# For instance,
+a="5"
+b="6"
+print(int(a)+int(b)) # Output: 11
+
+# Type of Typecasting: There are two type of typecasting.
+# Explicit Conversion: As a programer we are doing conversion.
+# For instance,
+string="10"
+number=3
+string_num=int(string)
+sum=string_num+number
+print("The sum of both is: ",sum) # Output: The sum of both is:  13
+
+# Implicit Conversion: Python do the conversion.
+# For instance,
+c=10.5
+d=6
+print(c+d) # Output: 16.5
+# Today, coding about the typecasting.
