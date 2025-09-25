254ec422204df2d910e6e12c976d064326849662
Arpita Sah <arpitasah750@gmail.com>
2025-09-25 21:24:13 +0545

    Create Day_7.py
    How to  take input in the python

---
+++ Day_7.py
@@ -0,0 +1,24 @@
+# Taking User Input in Python
+# In python, we can take user input directly using input() function.
+# This input function gives a return value as string/character hence we have to pass that into a cariable
+
+# Syntax of input()
+# variable=input()
+
+# But input function returns the value as string. Hence we have to typecaste them whenever required to another datatype.
+
+# For instance,
+# If you want to input the integer then, you use this syntax:
+num=int(input("Enter any number: "))
+print(num)
+
+# If you want to input float then, you can use this syntax:
+num1=float(input("Enter any float number: "))
+print(num1)
+
+# If you want to input teh string then, you can use this syntax:
+string=input("Enter you name ")
+print("My name is ",string)
+
+# So, you can input in python with the help of above rule as I applied above.
+# This is about the today coding about the input in python.
