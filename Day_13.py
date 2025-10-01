15537c25715c70944af65c9f69e524eac1635761
Arpita Sah <arpitasah750@gmail.com>
2025-10-01 21:18:09 +0545

    Create Day_13.py

---
+++ Day_13.py
@@ -0,0 +1,15 @@
+# In today coding I am going to code for the Loop in the Python.
+# Specially, I am going to code for the For Loop in Python.
+
+# Loops in Python
+# Sometimes a programmer wants to execute a group of statements a certain number of times. This can be done using loops.
+# Based on this loops are further classified into following types; for loop, while loop, nested loops.
+
+# For Loop
+# For loops can iterate over a sequence of iterable objects in Python.
+# Iterating over a sequence is nothing but iterating over strings,lists,tuples, sets and dictionaries.
+
+# Let's clear the for loop by doing some of the example:
+Name="Arpita Sah"
+for i in Name:
+    print(i, end="\n")
