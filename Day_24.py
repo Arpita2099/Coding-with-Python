6634c2bcd35b33cbd4c1d5312afecb0b115622a0
Arpita Sah <arpitasah750@gmail.com>
2025-10-12 22:38:14 +0545

    Create Day_24.py

---
+++ Day_24.py
@@ -0,0 +1,33 @@
+# Dictionary
+# Dictionary in Python is a collection of keys values, used to store data value like a map, which. unlike other data types which hold only a single value as an element.
+# In some languages it is known as map or assosiative arrays.
+# Characterstics
+# Mutable
+# Indexing has no meaning
+# Keys can't be duplicated
+# Key can't be multable items
+
+# Creating dictionary
+# empty dictionary
+d={}
+print(d)
+
+# 1D dictionary
+d1={'name':'Arpita','gender':'male'}
+print(d1)
+
+# with mixed key
+d2={(1,2,3,4,5):1,'Arpita':'Sah'}
+print(d2)
+
+# 2D Dictionary
+s={
+  'S_name':'Arpita Sah'
+  'College':'Presidential Graduate'
+  'trimester':'third trimester'
+  'Subject':{
+    'TEC':99
+    'PRG':100
+    'NET':100
+  }
+}
