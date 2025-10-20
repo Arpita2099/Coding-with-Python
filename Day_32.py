e9f555076b7162b35d3b21c7c5b0da423855d4b2
Arpita Sah <arpitasah750@gmail.com>
2025-10-20 21:53:28 +0545

    Add message translation program in Day_32.py
    Implement a program for encoding and decoding messages based on specified rules.

---
+++ Day_32.py
@@ -0,0 +1,15 @@
+# Write a python program to translate a message into secret code language, Use the rules
+# below to translate normal English into secret code language.
+
+# Coding:
+# If the word contains atleast 3 characters, remove the first letter and append at the end
+# now append three random characters at starting and the end
+# else:
+#   simply reverse the string
+
+# Decoding:
+# If the word contains less than 3 characters, reverse it
+# else:
+#   remove 3 random characters from start and end. Now remove the last letter and append it 
+# to the begning.
+# Youe program should ask whether you want to code or decode.
