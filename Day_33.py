df0482bdf1c3c444caa19677589beb3e7fda4698
Arpita Sah <arpitasah750@gmail.com>
2025-10-21 11:09:06 +0545

    Add shorthand if-else examples in Day_33.py
    Demonstrate shorthand if-else statements in Python.

---
+++ Day_33.py
@@ -0,0 +1,11 @@
+# Short hand if else
+# For instance:
+a=200
+b=300
+print("A") if a>b else print("=") if a==b else print("B")
+
+c=100 if a<b else 0
+print(c)
+
+num=19
+print(num,"is Even number.") if num%2==0 else print(num," is Odd number.")
