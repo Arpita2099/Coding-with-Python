e1ab4998f185f8a4c7f56bc1a961cf5339dd9b2a
Arpita Sah <arpitasah750@gmail.com>
2025-10-30 22:23:37 +0545

    Create Day_41.py

---
+++ Day_41.py
@@ -0,0 +1,21 @@
+# OS Module in Python
+# The Os module in Python is a built-in library that provides functions for interacting with
+# the opera5ting system. it allows you to perform a wide variety of tasks, such as reading 
+# and writing files, interacting with the file system, and running system commands.
+
+# Here are some common tasks you can perform with the os module:
+
+# Reading and writing files The os module provides functions for opening, reading, and 
+# writing files.
+# For instance, to open a file for reading, you can use the open function:
+
+# import os
+
+# Open the file in read-only mode.
+# f=os.open("myfile.txt",os.O_RDONLY)
+
+# Read the contents of the file
+# contents=os.read(f, 1024)
+
+# Close the file
+# os.close(f)
